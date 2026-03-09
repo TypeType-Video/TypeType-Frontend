8ccc2e649e933d5a2796b3bd27269249e4864e7c
Priveetee <brahim.benyoucefpro@proton.me>
2026-03-09 06:21:19 +0100

    refactor: extract nav items into dedicated module

--- apps/web/src/components/sidebar.tsx
+++ apps/web/src/components/sidebar.tsx
@@ -3,68 +3,8 @@ import { siBilibili, siNiconico, siYoutube } from "simple-icons";
 import { useSettings } from "../hooks/use-settings";
 import { useUiStore } from "../stores/ui-store";
 import type { ServiceId } from "../types/user";
+import { NAV_ITEMS } from "./nav-items";
 import { ServiceIcon } from "./service-icon";
-
-type NavItem = {
-  label: string;
-  to: string;
-  icon: React.ReactNode;
-};
-
-const NAV_ITEMS: NavItem[] = [
-  {
-    label: "Home",
-    to: "/",
-    icon: <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />,
-  },
-  {
-    label: "Trending",
-    to: "/trending",
-    icon: <polyline points="23 6 13.5 15.5 8.5 10.5 1 18" />,
-  },
-  {
-    label: "Subscriptions",
-    to: "/subscriptions",
-    icon: (
-      <>
-        <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9" />
-        <path d="M13.73 21a2 2 0 0 1-3.46 0" />
-      </>
-    ),
-  },
-  {
-    label: "History",
-    to: "/history",
-    icon: (
-      <>
-        <circle cx="12" cy="12" r="10" />
-        <polyline points="12 6 12 12 16 14" />
-      </>
-    ),
-  },
-  {
-    label: "Playlists",
-    to: "/playlists",
-    icon: (
-      <>
-        <path d="M3 5h15" />
-        <path d="M3 9h15" />
-        <path d="M3 13h9" />
-        <path d="M15 13l4 3-4 3V13z" />
-      </>
-    ),
-  },
-  {
-    label: "Settings",
-    to: "/settings",
-    icon: (
-      <>
-        <circle cx="12" cy="12" r="3" />
-        <path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06A1.65 1.65 0 0 0 4.68 15a1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06A1.65 1.65 0 0 0 9 4.68a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06A1.65 1.65 0 0 0 19.4 9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z" />
-      </>
-    ),
-  },
-];
 
 type Service = {
   id: ServiceId;
